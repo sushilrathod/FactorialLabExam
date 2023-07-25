d03e65b493c604c5d80e99c338076cbccddb014b
sushilrathod <sushil.rathod.1044@gmail.com>
2023-07-25 11:03:09 +0530

    factorila java commit

---
+++ Factorial.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Factorial{
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+    System.out.println("Enter number ");
+        int n=sc.nextInt();
+        int fact=1;
+
+        while(n>0){
+            fact=fact*n;
+            --n;
+        }
+        System.out.println(fact);
+    }
+}
